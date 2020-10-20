d0e7102c931a3394ac765f46c388b6ba00803495
Pengze Zheng <pzheng2@ucsc.edu>
2020-10-20 12:00:05 -0400

    add sun, moon, mouse detector

---
+++ HvG/Assets/Script/mouseClicker.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseClicker : MonoBehaviour
+{
+    public float force = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 100.0f))
+        {
+            if (hit.transform)
+            {
+                Rigidbody rb;
+
+                if (rb = hit.transform.GetComponent<Rigidbody>())
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        PrintName(hit.transform.gameObject);
+                        Push(rb);
+                    }
+                }
+            }
+        }
+    }
+
+    private void PrintName(GameObject go)
+    {
+        print(go.name);
+    }
+
+    private void Push(Rigidbody rig)
+    {
+        rig.AddForce(rig.transform.up * force, ForceMode.Impulse);
+    }
+}
---
+++ HvG/Assets/Script/sun.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float dayTime = 0;
+    float speed;
+    float height;
+    float width;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 1;
+        height = 10;
+        width = 12;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dayTime += Time.deltaTime * speed;
+        float x = Mathf.Cos(dayTime) * width;
+        float y = Mathf.Sin(dayTime) * height;
+        float z = 0;
+
+        transform.position = new Vector3(x, y, z);
+    }
+}
