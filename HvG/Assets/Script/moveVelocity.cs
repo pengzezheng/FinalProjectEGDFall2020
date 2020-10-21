28ee5c293ae3b1f9ae534beb10c863c448b702fb
Tilon Dixon <tilond2@gmail.com>
2020-10-21 01:55:01 -0400

    add

---
+++ HvG/Assets/Script/moveVelocity.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveVelocity : MonoBehaviour
+{
+    // Script to set movement speed
+    [SerializeField] float speed;
+    Vector3 velocityVector;
+    Rigidbody rb;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void SetVelocity(Vector3 velocity)
+    {
+        velocityVector = velocity;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = velocityVector * speed;
+    }
+}
---
+++ HvG/Assets/Script/playerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    // Move the player with the WASD keys
+    void Update()
+    {
+        float x = 0f;
+        float y = 0f;
+        
+        if (Input.GetKey(KeyCode.W)) y = 1f;
+        if (Input.GetKey(KeyCode.A)) x = -1f;
+        if (Input.GetKey(KeyCode.S)) y = -1f;
+        if (Input.GetKey(KeyCode.D)) x = 1f;
+
+        Vector3 moveVector = new Vector3(x, y).normalized;
+        GetComponent<moveVelocity>().SetVelocity(moveVector);
+    }
+}
